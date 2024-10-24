981d096235177b3dd9e5b3f1ac72a89d3683eb08
hmya_hnya <kh120205@gmail.com>
2024-10-24 17:53:45 +0900

    20241024 타임리프 문법으로 변환 필요

--- src/main/java/com/kh/HoneySpring/dao/UsersDAO.java
+++ src/main/java/com/kh/HoneySpring/dao/UsersDAO.java
@@ -1,21 +1,21 @@
 package com.kh.HoneySpring.dao;
 
 import com.kh.HoneySpring.vo.UsersVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+import javax.sql.DataSource;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class UsersDAO {
-    Connection conn = null;
-    Statement stmt = null;
+    static Connection conn = null;
+    static Statement stmt = null;
     PreparedStatement psmt = null;
-    ResultSet rs = null;
-    Scanner sc = new Scanner(System.in);
+    static ResultSet rs = null;
+    static Scanner sc = null;
 
-    // 회원가입
-    public void JoinUser() {
+    public void joinMember() {
         String userPW="", userID ="", nName, phone ="", pwLOCK, pwKey;
         // 아이디 생성
         while (true) {
@@ -116,7 +116,7 @@ public class UsersDAO {
         while (true) {
             System.out.println("전화번호를 입력 해 주세요(010-0000-0000, 형태로 하이픈을 포함하여 입력 해 주세요)");
             System.out.print("전화번호: ");
-            phone = inputPhone();
+            phone = sc.next();
             List<String> phonList = new ArrayList<>();
             try {
                 conn = Common.getConnection();  // 오라클 DB 연결
@@ -149,10 +149,11 @@ public class UsersDAO {
         }
         // 비밀번호 찾기 시 사용 할 질문 및 키워드
         while (true) {
+            sc.nextLine();
             System.out.println("비밀번호를 찾을 시 사용 할 질문을 입력 해 주세요");
             System.out.println("질문에는 비밀번호가 포함되어 있으면 안되며 한글 기준 20자 이내로 입력 해 주세요.");
             System.out.print("질문 입력: ");
-            pwLOCK = sc.next();
+            pwLOCK = sc.nextLine();
             if (pwLOCK.getBytes().length >= 60) {
                 continue;
             } else {
@@ -190,7 +191,7 @@ public class UsersDAO {
             psmt.setString(4, phone);
             psmt.setString(5, pwLOCK);
             psmt.setString(6, pwKey);
-            int ret = stmt.executeUpdate(query);
+            int ret = psmt.executeUpdate();
             System.out.println("Return: " + ret);
 
         } catch (SQLException e) {
@@ -201,18 +202,156 @@ public class UsersDAO {
     }
 
 
+    // !!!!!아이디 찾기------------------------------------------------------------------------------------------
+    public void findID() throws SQLException {
+        String userID;
+        Scanner sc = new Scanner(System.in);
+        String phone;
+        List<String> phoneList = new ArrayList<>();
+        try {
+            conn = Common.getConnection();  // 오라클 DB 연결
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("SELECT USERID FROM VM_LOGIN");
+            while(rs.next()){
+                String Phone = rs.getString("Phone");
+                phoneList.add(Phone);
+            }
+        } catch (Exception e){
+            System.out.println(e + " 연결 실패");
+        } finally {
+            Common.close(rs);
+            Common.close(stmt);
+            Common.close(conn);
+        }
+        while (true) {
+            System.out.println("가입 시 사용한 전화번호를 입력 해 주세요");
+            System.out.println("전화번호는 010-0000-0000 형식으로 하이픈을 포함하여 입력 해 주세요");
+            System.out.print("전화번호: ");
+            phone = inputPhone();
 
-
-
-// 메서드------------------------------------------------------------------------
-// 한글이 들어가지 않는지 확인하는 메서드(존재할 시 "" 리턴)
-public String noKor() {
-    String name = sc.next();
-    for (int i = 0; i < name.length(); i++) {
-        if (name.charAt(i) < 33 || name.charAt(i) > 126) return "";
+            if (phone.getBytes().length == 13) {
+            } else {
+                System.out.print("전화번호 입력 조건을 확인 후 다시 입력 해 주세요.");
+                continue;
+            }
+            try {
+                conn = Common.getConnection();
+                stmt = conn.createStatement();
+                rs = stmt.executeQuery("SELECT userID FROM USERS WHERE PHONE = '" + phone +"';");
+                if (rs.next()) {
+                    userID = rs.getString("USERID");
+                    String maskedID = maskUserID(userID);
+                    System.out.println("아이디는 " + maskedID + "입니다.");
+                } else {
+                    System.out.println("해당 전화번호로 가입된 아이디가 없습니다.");
+                }
+            }catch (Exception e) {
+                System.out.println(e + "연결 실패");
+                return;
+            }
+            break;
+        }
+        Common.close(rs);
+        Common.close(stmt);
+        Common.close(conn);
     }
-    return name;
-}
+
+
+    // !!!!!!비밀번호 찾기------------------------------------------------------------------------------
+    public void findPW() throws SQLException {
+        String userID, pwLOCK, pwKey, userPW;
+        Scanner sc = new Scanner(System.in);
+        List<String> IDList = new ArrayList<>();
+        try {
+            conn = Common.getConnection();  // 오라클 DB 연결
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("SELECT USERID FROM VM_LOGIN");
+            while(rs.next()){
+                String ID = rs.getString("USERID");
+                IDList.add(ID);
+            }
+        } catch (Exception e){
+            System.out.println(e + " 연결 실패");
+        } finally {
+            Common.close(rs);
+            Common.close(stmt);
+            Common.close(conn);
+        }
+        while (true) {
+            System.out.println("가입한 아이디를 입력 해 주세요");
+            System.out.print("아이디: ");
+            System.out.println();
+            userID = noKor();
+            if (rs.next()) {
+                userPW = rs.getString("USERPW");
+            } else {
+                System.out.println("해당 아이디로 가입된 계정이 없습니다.");
+                return;
+            }
+
+            if (userID.getBytes().length >= 8 && userID.getBytes().length <= 16) {
+            } else {
+                System.out.println("아이디 입력 조건을 다시 확인 해 주세요");
+                continue;
+            }
+            for (String e : IDList) {
+                if (IDList.equals(e)){
+                }
+                else {
+                }
+            }
+            break;
+        }
+
+        try {
+            conn = Common.getConnection();
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("SELECT pwLOCK FROM USERS WHERE userID = '" + userID +"';");
+            rs.next();
+            pwLOCK = rs.getString("pwLOCK");
+        } catch (Exception e) {
+            System.out.println(e + "연결 실패");
+            return;
+        }
+        while (true) {
+            System.out.println("제시문: " + pwLOCK);
+            // 데이터베이스에 있는 아이디 확인하여 pwLOCK 가져오기
+            System.out.println("키워드를 입력 해 주세요. 키워드는 한글 기준 8자 이하 입니다.");
+            System.out.print("키워드: ");
+            pwKey = sc.next();
+            if (pwKey.getBytes().length <= 24) {
+
+            } else {
+                System.out.println("키워드 입력 조건을 다시 확인 해 주세요");
+                continue;
+            }
+            try {
+                conn = Common.getConnection();
+                stmt = conn.createStatement();
+                rs = stmt.executeQuery("SELECT userPW FROM USERS WHERE pwKey = '" + pwKey +"';");
+                rs.next();
+                userPW = rs.getString("userPW");
+            }catch (Exception e) {
+                System.out.println(e + "연결 실패");
+                return;
+            }
+            System.out.print("비밀번호는 " + userPW + "입니다.");
+            break;
+        }
+        Common.close(rs);
+        Common.close(stmt);
+        Common.close(conn);
+    }
+
+    //---------------------------------------------------------------------------------------------------------------------
+    // 한글이 들어가지 않는지 확인하는 메서드(존재할 시 null리턴)
+    public String noKor() {
+        String name = sc.next();
+        for (int i = 0; i < name.length(); i++) {
+            if (name.charAt(i) < 33 || name.charAt(i) > 126) return "";
+        }
+        return name;
+    }
 
     // 숫자와 - 외의 문자가 들어가지 않는지 확인하는 메서드(존재할 시 null리턴)
     public String inputPhone() {
@@ -232,10 +371,6 @@ public String noKor() {
         String maskedPart = "*".repeat(userID.length() - 4);
         // 앞 4자를 제외한 문구는 *로 표기
         return visiblePart + maskedPart;
-    }
-
-    public List<UsersVO> selectUsersInfo() {
-        return List.of();
     }
 
 }
