e582988c5b5766e09a260b75730b77b9e771f15b
Cook <cook961004@gmail.com>
2024-10-25 14:04:11 +0900

    PostMakeDAO 작성

--- src/main/java/com/kh/HoneySpring/controller/PostMakeController.java
+++ src/main/java/com/kh/HoneySpring/controller/PostMakeController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @Controller
-@RequestMapping("/post")
+@RequestMapping("/posts")
 public class PostMakeController {
     private final PostMakeDAO postMakeDAO;
 
