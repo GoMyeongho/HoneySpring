c553486ce6748eefad0711debf06d240fb41037b
Cook <cook961004@gmail.com>
2024-10-25 17:33:02 +0900

    로그인 컨트롤러 클래스 생성 및 기초작업

--- src/main/java/com/kh/HoneySpring/controller/LoginController.java
+++ src/main/java/com/kh/HoneySpring/controller/LoginController.java
@@ -1,9 +1,21 @@
 package com.kh.HoneySpring.controller;
 
+import com.kh.HoneySpring.dao.LoginDAO;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/users")
 public class LoginController {
+    private final LoginDAO loginDAO;
+
+    public LoginController(LoginDAO loginDAO) {
+        this.loginDAO = loginDAO;
+    }
+
+    @PostMapping
+    public String logIn() {
+        return "Home";
+    }
 }
