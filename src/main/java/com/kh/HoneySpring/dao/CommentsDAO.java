0b3d3ce982954714e4a8801c1ac58441def4a27f
GoMyeongho <sdx02013@ajou.ac.kr>
2024-10-25 17:50:46 +0900

    DAO들

---
+++ src/main/java/com/kh/HoneySpring/dao/CommentsDAO.java
@@ -0,0 +1,83 @@
+package com.kh.HoneySpring.dao;
+
+import com.kh.HoneySpring.vo.CommentsVO;
+import com.kh.HoneySpring.vo.CommentsVO;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+@Repository
+public class CommentsDAO {
+    private final JdbcTemplate jdbcTemplate;
+    
+    public CommentsDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<CommentsVO> commList (int postNo) {
+        String sql = "SELECT * FROM VM_COMM WHERE POSTNO = ?";
+        List<CommentsVO> result = null;
+        try {
+            result = jdbcTemplate.query(sql,new Object[]{postNo}, new CommentsRowMapper());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return result;
+    }
+
+    public boolean addComment(CommentsVO vo, int commNo) {
+        String sql = "INSERT INTO COMMENTS (COMMNO, SUBNO, POSTNO, USERID, CCONTENT, CDATE) VALUES(?, SEQ_SUBNO.NEXTVAL, ?, ?, ?, SYSDATE)";
+        int result = 0;
+        try {
+
+            result = jdbcTemplate.update(sql, commNo, vo.getPostNo(), vo.getUserId(), vo.getContent());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return result > 0;
+    }
+    public boolean addComment(CommentsVO vo) {
+        String sql = "INSERT INTO COMMENTS (COMMNO, SUBNO, POSTNO, USERID, CCONTENT, CDATE) VALUES(SEQ_COMMNO.NEXTVAL, SEQ_SUBNO.NEXTVAL, ?, ?, ?, SYSDATE)";
+        int result = 0;
+        try {
+
+            result = jdbcTemplate.update(sql, vo.getPostNo(), vo.getUserId(), vo.getContent());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return result > 0;
+    }
+
+    public boolean Comment(int postNo, String id) {
+        String sql = "INSERT INTO COMMENTS (COMMNO, SUBNO, POSTNO, USERID, CCONTENT, CDATE) VALUES(SEQ_COMMNO.NEXTVAL, SEQ_SUBNO.NEXTVAL, ?, ?, ?, SYSDATE)";
+        int result = 0;
+        try {
+            result = jdbcTemplate.update(sql, id, postNo);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return result > 0;
+    }
+
+
+    private static class CommentsRowMapper implements RowMapper<CommentsVO> {
+
+        @Override
+        public CommentsVO mapRow(ResultSet rs, int rowNum) throws SQLException {
+            return new CommentsVO(
+                    rs.getInt("POSTNO"),
+                    rs.getString("NNAME"),
+                    rs.getString("CCONTENT"),
+                    rs.getDate("CDATE"),
+                    rs.getInt("COMMNO"),
+                    rs.getInt("SUBNO"),
+                    rs.getString("USERID")
+            );
+        }
+    }
+
+}
