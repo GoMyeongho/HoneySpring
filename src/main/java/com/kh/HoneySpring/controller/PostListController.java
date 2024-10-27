e4dfdbf18462424f0012cf052f31facc7bc099c4
myeongho <sdx02013@ajou.ac.kr>
2024-10-27 22:41:09 +0900

    컨트롤러

--- src/main/java/com/kh/HoneySpring/controller/PostListController.java
+++ src/main/java/com/kh/HoneySpring/controller/PostListController.java
@@ -1,29 +1,41 @@
 package com.kh.HoneySpring.controller;
 
 
+import com.kh.HoneySpring.dao.LikesDAO;
 import com.kh.HoneySpring.dao.PostListDAO;
+import com.kh.HoneySpring.vo.LikesVO;
 import com.kh.HoneySpring.vo.PostsVO;
+import com.kh.HoneySpring.vo.UsersVO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 @RequestMapping("/posts")
 public class PostListController {
     private final PostListDAO dao;
+    private final LikesDAO lDao;
+    private final LikesDAO likesDAO;
 
-    public PostListController(PostListDAO dao) {
+    public PostListController(PostListDAO dao, LikesDAO lDao, LikesDAO likesDAO) {
         this.dao = dao;
+        this.lDao = lDao;
+        this.likesDAO = likesDAO;
     }
 
     @GetMapping("/board")    // http://localhost:8112/posts/list
-    public String showBoard(Model model) {
+    public String showBoard(@ModelAttribute('login')UsersVO vo, Model model) {
         List<PostsVO> board = dao.selectPage();
         List<String> categories = List.of("Health", "Travel", "Life", "Cook", "Q&A");
         List<String> searchOptions = List.of("제목", "내용", "작성자", "제목 + 내용");
+        String id = vo.getUserID();
+        List<LikesVO> like = lDao.likeList(id);
+        for (PostsVO post : board) post.setTitle(post.getTitle() +"[" + lDao.likeMark(like, post.getPostno()) +"]");
         model.addAttribute("searchOptions", searchOptions);
         model.addAttribute("categories", categories);
         model.addAttribute("board", board);
--- src/main/java/com/kh/HoneySpring/controller/PostViewController.java
+++ src/main/java/com/kh/HoneySpring/controller/PostViewController.java
@@ -2,7 +2,6 @@ package com.kh.HoneySpring.controller;
 
 import com.kh.HoneySpring.dao.CommentsDAO;
 import com.kh.HoneySpring.dao.LikesDAO;
-import com.kh.HoneySpring.dao.PostListDAO;
 import com.kh.HoneySpring.dao.PostViewDAO;
 import com.kh.HoneySpring.vo.CommentsVO;
 import com.kh.HoneySpring.vo.LikesVO;
@@ -17,41 +16,47 @@ import java.util.List;
 @RequestMapping("/board")
 public class PostViewController {
     private final PostViewDAO dao;
-    private final LikesDAO ldao;
-    private final CommentsDAO cdao;
+    private final LikesDAO lDao;
+    private final CommentsDAO cDao;
 
     public PostViewController(PostViewDAO dao , LikesDAO ldao , CommentsDAO cdao) {
         this.dao = dao;
-        this.ldao = ldao;
-        this.cdao = cdao;
+        this.lDao = ldao;
+        this.cDao = cdao;
     }
 
     @GetMapping("/view")    // http://localhost:8112/posts/list
-    public String viewPost(@RequestParam int postNo, Model model) {
+    public String viewPost(@ModelAttribute("postSelect") int postNo, Model model) {
         PostsVO post= dao.viewPost(postNo);
-        List<LikesVO> lList = ldao.likeList(postNo);
-        List<CommentsVO> cList = cdao.commList(postNo);
+        List<LikesVO> lList = lDao.likeList(postNo);
+        List<CommentsVO> cList = cDao.commList(postNo);
         model.addAttribute("post", post);
         model.addAttribute("lList", lList);
         model.addAttribute("cList", cList);
         return "thymeleaf/viewPost";
     }
+
     @GetMapping("/update")
-    public String updatePost(@ModelAttribute("post"), Model model) {
+    public String updatePost(@ModelAttribute("post") PostsVO vo, Model model) {
         List<String> categories = List.of("Health", "Travel", "Life", "Cook", "Q&A");
         model.addAttribute("categories", categories);
+        model.addAttribute("post", vo);
         return "thymeleaf/updatePost";
     }
 
     @PostMapping("/update")
-    public String submitUpdatePost(@ModelAttribute("post"), PostsVO vo, Model model) {
+    public String submitUpdatePost(@ModelAttribute("post") PostsVO vo, Model model) {
         boolean success = dao.updatePost(vo);
         model.addAttribute("success", success);
         return "thymeleaf/submitUpdatePost";
     }
 
     @PostMapping("/view")
-    public String deletePost(@RequestParam int postNO, Model model) {
-        boolean success = dao.deletePost();
+    public String submitDeletePost(@ModelAttribute("post") PostsVO vo, Model model) {
+        boolean success = dao.deletePost(vo.getPostno());
+        model.addAttribute("success", success);
+        return "thymeleaf/submitDeletePost";
     }
+
+
 }
