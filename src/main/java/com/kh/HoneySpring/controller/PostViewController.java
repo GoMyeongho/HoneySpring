39eb46c3ef6a8af2cfce50697e82668788d36b77
GoMyeongho <sdx02013@ajou.ac.kr>
2024-10-28 17:50:05 +0900

    controller 설정

--- src/main/java/com/kh/HoneySpring/controller/PostViewController.java
+++ src/main/java/com/kh/HoneySpring/controller/PostViewController.java
@@ -10,12 +10,13 @@ import com.kh.HoneySpring.vo.UsersVO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Collections;
 import java.util.List;
 
 @Controller
-@RequestMapping("/board")
+@RequestMapping("/posts")
 public class PostViewController {
     private final PostViewDAO dao;
     private final LikesDAO lDao;
@@ -50,7 +51,7 @@ public class PostViewController {
         return "thymeleaf/viewPost";
     }
 
-    @GetMapping("view/update")
+    @GetMapping("/update")
     public String updatePost(@RequestParam("post") PostsVO vo, Model model) {
         List<String> categories = List.of("Health", "Travel", "Life", "Cook", "Q&A");
         model.addAttribute("categories", categories);
@@ -58,18 +59,57 @@ public class PostViewController {
         return "thymeleaf/updatePost";
     }
 
-    @PostMapping("view/update")
-    public String submitUpdatePost(@ModelAttribute("post") PostsVO vo, Model model) {
+    @PostMapping("/update")
+    public String submitUpdatePost(@ModelAttribute("post") PostsVO vo, RedirectAttributes redirectAttributes) {
         boolean success = dao.updatePost(vo);
-        model.addAttribute("success", success);
-        return "thymeleaf/submitUpdatePost";
+        redirectAttributes.addFlashAttribute("updateSuccess", success);
+        return "redirect:/posts/view";
+    }
+    @PostMapping("/delete")
+    public String deletePost(@ModelAttribute("post") PostsVO vo, RedirectAttributes redirectAttributes) {
+        boolean success = dao.deletePost(vo.getPostno());
+        redirectAttributes.addFlashAttribute("deleteSuccess", success);
+        return "redirect:/posts/view";
     }
 
-    @PostMapping("/view/delete")
-    public String submitDeletePost(@ModelAttribute("post") PostsVO vo, Model model) {
-        boolean success = dao.deletePost(vo.getPostno());
-        model.addAttribute("success", success);
-        return "thymeleaf/submitDeletePost";
+    @GetMapping("/comment/create")
+    public String createComment(@ModelAttribute("login")UsersVO vo, @RequestParam("commType")String type, @RequestParam(value = "commNo", required = false) int commNo, Model model) {
+        CommentsVO comm = new CommentsVO();
+        comm.setCommNo((type.equals("type1") ? 0 : commNo));
+        comm.setNName(vo.getNName());
+        comm.setUserId(vo.getUserID());
+        model.addAttribute("comment", new CommentsVO());
+        model.addAttribute("commType", type);
+        return "thymeleaf/createComment";
+    }
+
+    @PostMapping("/comment/create")
+    public String submitComment(@RequestParam("comment") CommentsVO vo, @RequestParam(value = "commNo", defaultValue = "0") int commNo, RedirectAttributes redirectAttributes) {
+        boolean success;
+        if (commNo == 0) success = cDao.addComment(vo);
+        else success = cDao.addComment(vo, commNo);
+        redirectAttributes.addFlashAttribute("createCommSuccess", success);
+        return "redirect:/posts/view";
+    }
+
+    @GetMapping("/comment/update")
+    public String updateComment(@RequestParam("comment") CommentsVO vo, Model model) {
+        model.addAttribute("comment", vo);
+        return "thymeleaf/updateComment";
+    }
+
+    @PostMapping("/comment/update")
+    public String submitUpdateComment(@RequestParam("comment") CommentsVO vo, RedirectAttributes redirectAttributes) {
+        boolean success = cDao.updateComment(vo);
+        redirectAttributes.addFlashAttribute("updateCommSuccess", success);
+        return "redirect:/posts/view";
+    }
+
+    @PostMapping("/comment/delete")
+    public String submitDeleteComment(@RequestParam("comment") CommentsVO vo, RedirectAttributes redirectAttributes) {
+        boolean success = cDao.deleteComment(vo);
+        redirectAttributes.addFlashAttribute("deleteCommSuccess", success);
+        return "redirect:/posts/view";
     }
 
 
