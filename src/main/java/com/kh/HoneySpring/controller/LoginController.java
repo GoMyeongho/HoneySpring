ad8d45b9841b5371eca28ba34161ea304e8372ea
Cook <cook961004@gmail.com>
2024-10-28 17:42:17 +0900

    로그인 컨트롤러 수정 및 스타일 수정

--- src/main/java/com/kh/HoneySpring/controller/LoginController.java
+++ src/main/java/com/kh/HoneySpring/controller/LoginController.java
@@ -6,6 +6,7 @@ import com.kh.HoneySpring.vo.PostsVO;
 import com.kh.HoneySpring.vo.UsersVO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,8 +26,9 @@ public class LoginController {
     UsersVO dbBasedUser = loginDAO.FindByUserID(usersVO.getUserID());
         if (dbBasedUser != null && usersVO.getUserPW().equals(usersVO.getUserPW())) {
             model.addAttribute("userID", usersVO.getUserID());
-            return "Thymeleaf/posts"; // 로그인 후에 포스트 페이지로 전달
+            return "Thymeleaf/showBoard"; // 로그인 후에 포스트 페이지로 전달
         } else {
+            model.addAttribute("login", new UsersVO());
             model.addAttribute("에러", "아이디, 비밀번호가 올바르지 않습니다.");
             return "Thymeleaf/login"; // 로그인 페이지 유지
         }
@@ -39,17 +41,17 @@ public class LoginController {
     }
 
     @GetMapping("/findID") // html 에서 버튼 클릭시 아이디찾기 페이지로
-    public String findID(Model model) {    // http://localhost:8112/findIDForm
-        return "Thymeleaf/findIDForm";
+    public String findID(Model model) {    // http://localhost:8112/findID
+        return "Thymeleaf/findID";
     }
 
     @GetMapping("/findPW") // html 에서 버튼 클릭시 비밀번호 찾기 페이지로
-    public String findPW(Model model) {    // http://localhost:8112/findPWForm
-        return "Thymeleaf/findPWForm";
+    public String findPW(Model model) {    // http://localhost:8112/findPW
+        return "Thymeleaf/findPW";
     }
 
     @GetMapping("/signUp") // html 에서 버튼 클릭시 회원가입 페이지로
-    public String signUp(Model model) {    // http://localhost:8112/findjoinForm
-        return "Thymeleaf/joinForm";
+    public String signUp(Model model) {    // http://localhost:8112/joinUser
+        return "Thymeleaf/joinUser";
     }
 }
