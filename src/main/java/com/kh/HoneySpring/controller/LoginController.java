5abbca54b5942bb4ce9bfe7f4534d35d306732bb
Cook <cook961004@gmail.com>
2024-10-28 09:38:17 +0900

    로그인 컨트롤러 생성

--- src/main/java/com/kh/HoneySpring/controller/LoginController.java
+++ src/main/java/com/kh/HoneySpring/controller/LoginController.java
@@ -1,7 +1,10 @@
 package com.kh.HoneySpring.controller;
 
 import com.kh.HoneySpring.dao.LoginDAO;
+import com.kh.HoneySpring.dao.UsersDAO;
+import com.kh.HoneySpring.vo.UsersVO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,8 +17,15 @@ public class LoginController {
         this.loginDAO = loginDAO;
     }
 
-    @PostMapping
-    public String logIn() {
-        return "Home";
+    @PostMapping("/login")
+    public String logIn(String userID, Model model) {   // http://localhost:8112/users/login
+        UsersVO usersVO = loginDAO.FindByUserID(userID);
+        if (usersVO != null) {  // 로그인에 Null 값이 아닌 정상적인 값
+            model.addAttribute("User", userID);
+            return "main"; // 로그인 후에 메인 페이지로 반환
+        } else {
+            model.addAttribute("에러", "아이디, 비밀번호가 올바르지 않습니다.");    // 정상적이지 않은 값
+            return "login"; // 로그인페이지 유지
+        }
     }
 }
