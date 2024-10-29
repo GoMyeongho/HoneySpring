193dea70bfa939ec4dc935085129cc923f2e4927
GoMyeongho <sdx02013@ajou.ac.kr>
2024-10-29 11:48:48 +0900

    controller 설정

--- src/main/java/com/kh/HoneySpring/vo/UsersVO.java
+++ src/main/java/com/kh/HoneySpring/vo/UsersVO.java
@@ -12,9 +12,20 @@ import java.sql.Date;
 public class UsersVO {
     private String userID;
     private String userPW;
+    private String confirmPW;
     private String nName;
     private String phone;
     private Date updateDATE;
     private String pwLOCK;
     private String pwKey;
+
+    public UsersVO(String userID, String userPW, String nName, String phone, Date updateDATE, String pwLOCK, String pwKey) {
+        this.userID = userID;
+        this.userPW = userPW;
+        this.nName = nName;
+        this.phone = phone;
+        this.updateDATE = updateDATE;
+        this.pwLOCK = pwLOCK;
+        this.pwKey = pwKey;
+    }
 }
