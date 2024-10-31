78a3300f406621501b6338bafd3c3d97720c3494
GoMyeongho <sdx02013@ajou.ac.kr>
2024-10-31 12:29:55 +0900

    controller 설정

--- src/main/java/com/kh/HoneySpring/controller/LoginController.java
+++ src/main/java/com/kh/HoneySpring/controller/LoginController.java
@@ -32,34 +32,34 @@ public class LoginController {
             return "redirect:/posts/board"; // 로그인 성공 시 posts/board 로 리디렉션
         } else {
             model.addAttribute("error", "아이디, 비밀번호가 올바르지 않습니다.");   // 로그인 실패시 에러표시
-            return "Thymeleaf/login"; // 실패 시 로그인 페이지로
+            return "thymeleaf/login"; // 실패 시 로그인 페이지로
         }
     }
 
     @GetMapping("/board")
     public String board() {
-        return "Thymeleaf/showBoard";
+        return "thymeleaf/showBoard";
     }
 
     @GetMapping("/login")
     public String login(Model model, HttpSession session) {
         session.removeAttribute("login");
         model.addAttribute("login", new UsersVO());
-        return "Thymeleaf/login";
+        return "thymeleaf/login";
     }
 
     @GetMapping("/findID") // html 에서 버튼 클릭시 아이디찾기 페이지로
     public String findID(Model model) {    // http://localhost:8112/findID
-        return "Thymeleaf/findID";
+        return "thymeleaf/findID";
     }
 
     @GetMapping("/findPW") // html 에서 버튼 클릭시 비밀번호 찾기 페이지로
     public String findPW(Model model) {    // http://localhost:8112/findPW
-        return "Thymeleaf/findPW";
+        return "thymeleaf/findPW";
     }
 
     @GetMapping("/signUp") // html 에서 버튼 클릭시 회원가입 페이지로
     public String signUp(Model model) {    // http://localhost:8112/joinUser
-        return "Thymeleaf/signUp";
+        return "thymeleaf/signUp";
     }
 }
